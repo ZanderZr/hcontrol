3b59c3b52d67f7453d8c337b9a9a780d72b41bce
Juan Carlos <jc.zr88@gmail.com>
2025-01-23 15:12:30 +0100

    visual del diary a medias

--- frontend/src/app/modules/mental/mental-diary/mental-diary.component.ts
+++ frontend/src/app/modules/mental/mental-diary/mental-diary.component.ts
@@ -1,12 +1,33 @@
 import { Component } from '@angular/core';
+import { PageComponent } from "../../../components/page/page.component";
 
 @Component({
   selector: 'app-mental-diary',
   standalone: true,
-  imports: [],
+  imports: [PageComponent],
   templateUrl: './mental-diary.component.html',
   styleUrl: './mental-diary.component.scss'
 })
 export class MentalDiaryComponent {
+  day: string = "";
+
+  constructor() {
+    this.day = this.getToday();
+  }
+
+  getToday(): string {
+    const today = new Date();
+    const dayName = today.toLocaleDateString('es-ES', { weekday: 'long' }); // Cambia 'es-ES' por el idioma deseado
+    return dayName.charAt(0).toUpperCase() + dayName.slice(1);
+  }
+
+  getDate(): string {
+    const today = new Date();
+    return today.toLocaleDateString('es-ES', {
+      day: 'numeric',
+      month: 'long',
+      year: 'numeric',
+    });
+  }
 
 }
